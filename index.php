5ac672dd55c67e5735d90f74159847b5ff743862
SOUKAKHOM MOUNTHADY <soukakom2018@gmail.com>
2025-05-16 01:35:22 +0700

    Your message

--- index.php
+++ index.php
@@ -6,47 +6,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Notes Management System</title>
+        <title>Notes Management App</title>
         <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="registration.php">
-                                <div class="sb-nav-link-icon"><i class="fa fa-user"></i></div>
-                                Registration
-                            </a>
-                            <a class="nav-link" href="login.php">
-                                <div class="sb-nav-link-icon"><i class="fa fa-user"></i></div>
-                                Login
-                            </a>
-                        </div>
-                    </div>
-                </nav>
+        <!-- Responsive Top Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Notes Management App</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="registration.php"><i class="fa fa-user"></i> Registration</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php"><i class="fa fa-sign-in-alt"></i> Login</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Notes Management System</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"></li>
-                        </ol>
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    <img src="assets/img/banner.jpg"  height="550">
-                                </p>
-                            </div>
-                        </div>
+        </nav>
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4 text-center">Notes Management App</h1>
+                <ol class="breadcrumb mb-4 justify-content-center">
+                    <li class="breadcrumb-item"></li>
+                </ol>
+                <div class="card mb-4">
+                    <div class="card-body d-flex justify-content-center align-items-center">
+                        <img src="assets/img/banner.jpg" class="img-fluid rounded" alt="Banner" style="max-height: 400px; width: 100%; object-fit: cover;">
                     </div>
-                </main>
+                </div>
             </div>
-        </div>
+        </main>
         <?php include_once('includes/footer.php');?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
