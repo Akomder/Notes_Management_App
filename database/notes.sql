2f025e1317d5d849f330aebb4daa4dfab699efa2
SOUKAKHOM MOUNTHADY <soukakom2018@gmail.com>
2025-05-16 23:53:01 +0700

    updated

--- database/notes.sql
+++ database/notes.sql
@@ -1,13 +1,12 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+07:00";
 
 CREATE TABLE `tblregistration` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `firstName` varchar(150) DEFAULT NULL,
   `lastName` varchar(150) DEFAULT NULL,
   `emailId` varchar(150) DEFAULT NULL,
-  `username` varchar(60) DEFAULT NULL,
   `mobileNumber` bigint(12) DEFAULT NULL,
   `userPassword` varchar(255) DEFAULT NULL,
   `regDate` timestamp NULL DEFAULT current_timestamp()
@@ -15,7 +14,7 @@ CREATE TABLE `tblregistration` (
 
 CREATE TABLE `tblcategory` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `categoryName` varchar(125) DEFAULT NOT NULL,
+  `categoryName` varchar(125) DEFAULT NULL,
   `createdBy` int(5) DEFAULT NULL,
   `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -23,20 +22,11 @@ CREATE TABLE `tblcategory` (
 CREATE TABLE `tblnotes` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `noteCategory` varchar(255) DEFAULT NULL,
-<<<<<<< HEAD
-  `noteContent` TEXT DEFAULT NULL,
   `noteTitle` varchar(255) DEFAULT NULL,
   `noteDescription` mediumtext DEFAULT NULL,
+  `noteImage` varchar(255) DEFAULT NULL,
   `createdBy` int(5) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `noteImage` varchar(255) DEFAULT NULL
-=======
-  `noteContent` TEXT DEFAULT NOT NULL,
-  `noteTitle` varchar(255) DEFAULT NOT NULL,
-  `noteDescription` mediumtext DEFAULT NOT NULL,
-  `createdBy` int(5) DEFAULT NOT NULL,
   `creationDate` timestamp NULL DEFAULT current_timestamp()
->>>>>>> 5e791b3c458d2d1f1a8edeae3a9fc72cfc283726
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `tblnoteshistory` (
